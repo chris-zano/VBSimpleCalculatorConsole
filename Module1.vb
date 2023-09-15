756efa69c3e481e459f5026e9d64fbacd78f57a3
Christian Solomon <cncs.chris@gmail.com>
2023-09-15 09:10:09 +0000

    initialise repository

---
+++ Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+    Sub Main()
+        'Assignment - create a program called simple calculator, that will receive 
+        '   two numbers from the user, add them together and display the output on the screen
+        Console.WriteLine("Enter the first number: ")
+        Dim x As Long = Console.ReadLine()
+        Console.WriteLine("Enter the second number: ")
+        Dim y As Long = Console.ReadLine()
+        Dim sum As Long = x + y
+
+        Console.WriteLine($"{x} + {y} = {sum}")
+        Console.ReadKey(True)
+    End Sub
+End Module
